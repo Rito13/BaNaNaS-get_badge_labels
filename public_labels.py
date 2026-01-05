aeb78f54d0414ea8c9b22c09422eabb83ad0012c
Cyprian Klimaszewski <kapitan.cypek@gmail.com>
2026-01-05 13:43:34 +0100

    Add: Storage for public labels.

---
+++ public_labels.py
@@ -0,0 +1,10 @@
+PUBLIC_LABELS = {
+	"power/battery": [-1, 2025, 2, 27, "Battery"],
+	"power/diesel": [-1, 2025, 2, 27, "Diesel"],
+	"power/electric": [-1, 2025, 2, 27, "Electric"],
+	"power/steam": [-1, 2025, 2, 27, "Steam"],
+	"power/turbine": [-1, 2025, 2, 27, "Gas Turbine"],
+	"power/electric/ac": [-1, 2025, 2, 27, "Electric (AC)"],
+	"power/electric/dc": [-1, 2025, 2, 27, "Electric (DC)"],
+	"power/diesel/diesel-hydraulic": [-1, 2025, 2, 27, "Diesel (Diesel-hydraulic)"],
+}
