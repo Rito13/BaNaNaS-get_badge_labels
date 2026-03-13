be3642ac31c7581eb04d35fea2c942123c690ac6
Cyprian Klimaszewski <kapitan.cypek@gmail.com>
2026-03-13 14:48:08 +0100

    Change: Make client autonomus.

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@ from threading import Thread, RLock
 from os import _exit
 from queue import Queue
 from sys import argv
+from time import sleep
 # from Library import decode_error
 
 lock = RLock()
@@ -84,7 +85,7 @@ def decode_grf_info(grf_info):
 	lock.release()
 
 
-def save_grf(grf_data):
+def save_grf(grf_data, report_queue):
 	global grf_file
 	lock.acquire()
 	if grf_file == "":
@@ -97,7 +98,7 @@ def save_grf(grf_data):
 		file_name = read_string(12, grf_data)
 		print(content_id, filesize, file_name)
 		lock.acquire()
-		grf_file = file_name + ".tar"
+		grf_file = f"grf_tars/{file_name}.tar"
 		open(grf_file, "bw")  # Clear old content.
 		lock.release()
 		return
@@ -105,13 +106,14 @@ def save_grf(grf_data):
 		grf_file = ""
 		lock.release()
 		print("Download completed!")
+		report_queue.put(True)
 		return
 	with open(grf_file, "ba") as f:
 		lock.release()
 		f.write(bytes(grf_data[3:]))
 
 
-def decoder(soc):
+def decoder(soc, report_queue):
 	open("out.txt", "w")  # reset file
 	try:
 		length = 0
@@ -132,7 +134,7 @@ def decoder(soc):
 								case 4:
 									decode_grf_info(packet)
 								case 6:
-									save_grf(packet)
+									save_grf(packet, report_queue)
 								case _:
 									print(f"Unknown packet type {packet[2]}")
 						f.write("\n")
@@ -146,29 +148,45 @@ def client_program():
 	port = 3978  # socket server port number
 	client_socket = socket.socket()  # instantiate
 	client_socket.connect((host, port))
-	t1 = Thread(target=decoder, args=(client_socket,))
+
+	report_queue = Queue()
+	t1 = Thread(target=decoder, args=(client_socket, report_queue))
 	t1.start()
-	message = input(" -> ")  # take input
-	while True:
-		match message.lower().strip():
-			case "get_info":
-				versions = ["vanilla", "100.0", "jgrpp", "100.0"]  # Big number version to prevent updating it with each new release.
-				version_bytes = bytes([0]).join([s.encode() for s in versions + [""]])
-				client_socket.send(bytes([*bytes_from_int(9 + len(version_bytes), 2), 0, 2, 255, 255, 255, 255, len(versions) // 2]) + version_bytes)
-			case "download":
-				user_input = input("What? -> ")
-				what = []
-				for s in user_input.split(","):
-					what += bytes_from_int(int(s), 4)
-				print(*what)
-				client_socket.send(bytes([*bytes_from_int(5 + len(what), 2), 5, *bytes_from_int(len(what) // 4, 2), *what]))
-			case "get_pls":
-				lock.acquire()
-				print(GRF_IDS["0x50530001"])
+
+	versions = ["vanilla", "100.0", "jgrpp", "100.0"]  # Big version numbers to prevent updating it with each new release.
+	version_bytes = bytes([0]).join([s.encode() for s in versions + [""]])
+	client_socket.send(bytes([*bytes_from_int(9 + len(version_bytes), 2), 0, 2, 255, 255, 255, 255, len(versions) // 2]) + version_bytes)
+	sleep(1)  # Wait for info to be received.
+
+	contents = []
+	ATTEMPTS = 5  # Do only 5 attempts.
+
+	for unique_id in argv[1:]:
+		if unique_id[1] != "x":
+			unique_id = f"0x{unique_id}"
+		for i in range(ATTEMPTS):
+			lock.acquire()
+			if unique_id in GRF_IDS:
+				contents.append(GRF_IDS[unique_id])
 				lock.release()
-			case "exit":
 				break
-		message = input(" -> ")  # again take input
+			lock.release()
+			if i == ATTEMPTS - 1:
+				print(f"Could not find {unique_id} in databse.")
+				break
+			sleep(2**i)  # After each unsuccessful attempt sleep more.
+
+	what = []
+	for id in contents:
+		what += bytes_from_int(id, 4)
+	print(*what)
+	client_socket.send(bytes([*bytes_from_int(5 + len(what), 2), 5, *bytes_from_int(len(what) // 4, 2), *what]))
+
+	while len(contents) > 0:
+		report_queue.get()
+		contents = contents[1:]
+
+	sleep(1)  # Give decoder some time, to clean.
 	client_socket.close()  # close the connection
 	_exit(0)
 
